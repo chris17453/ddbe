b8f2dfcf1cc6181164f1d03069e376bea80a215f
Charles Watkins <chris17453@gmail.com>
2020-05-19 13:58:50 -0400

    Commiting Build 0.0.0

--- commit.sh
+++ commit.sh
@@ -1,23 +1,46 @@
 #!/bin/bash
+# increment a version number for a build 
+# and commit the contents of a repository 
+# with that a version
+
+# version file
+# 1.0.0
 version=source/version.txt
+# min and max for each variable
+# incase you want to limit range
+MAJOR_MIN=0
+MINOR_MIN=0
+PATCH_MIN=0
+MAJOR_MAX=999
+MINOR_MAX=999
+PATCH_MAX=999
+MAJOR_INC=1
+MINOR_INC=1
+PATCH_INC=1
 
 
-# This gets the curent version.. and bumps the patch +1
-major=$(cat $version| cut -d '.' -f 1)
-minor=$(cat $version| cut -d '.' -f 2)
-patch=$(cat $version| cut -d '.' -f 3)
+if [[ -f "$FILE" ]]; then
+    # This gets the curent version.. and bumps the patch +1
+    major=$(cat $version| cut -d '.' -f 1)
+    minor=$(cat $version| cut -d '.' -f 2)
+    patch=$(cat $version| cut -d '.' -f 3)
+    # overflow logic
+    patch=$((patch+PATCH_INC))
+    if [[ $patch -gt $PATCH_MAX ]]; 
+    then 
+    patch=#PATCH_MIN
+    minor=$((minor+MINOR_INC))
+    fi
+    if [[ $minor -gt $MINOR_MAX ]]; 
+    then 
+    minor=$MAJOR_MIN
+    major=$((major+MAJOR_INC))
+    fi
+else
+    major=$MAJOR_MIN
+    minor=$MINOR_MIN
+    patch=$PATCH_MIN
 
-# overflow logic
-patch=$((patch+1))
-if [[ $patch -gt 999 ]]; 
-then 
-  patch=0
-  minor=$((minor+1))
-fi
-if [[ $minor -gt 999 ]]; 
-then 
-  minor=0
-  major=$((major+1))
 fi
 build=$major.$minor.$patch
 
