525692a781c9882430b50064c9222d2a210c73b6
Charles Watkins <chris17453@gmail.com>
2020-05-25 16:58:50 -0400

    Commiting Build 1.0.213

--- source/parser/simple/alias.c
+++ source/parser/simple/alias.c
@@ -14,7 +14,7 @@ int expr_alias(token_array_t *tokens,int depth,char *target){
         if(compare_token(tokens,0,TOKEN_ALPHA)){        
             printf("*COMP-> %d %s\n",pos+1,tokens->array[pos+1].value);
             *target=tokens->array[pos].value;
-            printf("*TARGET-> %s\n",target);
+            printf("*TARGET-> %s\n",*target);
             //target=&tokens->array[tokens->position-1].value;
          //   set_alias(tokens,tokens->position-1,target);
             return 1;
