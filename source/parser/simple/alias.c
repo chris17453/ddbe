71e743b7c0080fa226d7f01a8e54c4b45ba71b8b
Charles Watkins <chris17453@gmail.com>
2020-05-25 16:37:02 -0400

    Commiting Build 1.0.172

--- source/parser/simple/alias.c
+++ source/parser/simple/alias.c
@@ -7,12 +7,12 @@ int expr_alias(token_array_t *tokens,int depth,char **target){
     #ifdef PARSE_ENTRANCE
     goop(depth,"ALIASEXPR","IN");
     #endif
-    target=&"bob";
+    
     
     int pos=tokens->position;
     if(compare_token(tokens,0,TOKEN_AS))
         if(compare_token(tokens,0,TOKEN_ALPHA)){        
-            target=&"bob:";
+            target=&tokens->array[tokens->position-1].value;
             //target=&tokens->array[tokens->position-1].value;
             set_alias(tokens,tokens->position-1,target);
             return 1;
