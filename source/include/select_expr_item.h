37f42109454ef848b27de91d1ff147a0d2e5a5d8
Charles Watkins <chris17453@gmail.com>
2020-05-25 13:40:01 -0400

    Commiting Build 1.0.49

--- source/include/select_expr_item.h
+++ source/include/select_expr_item.h
@@ -1,7 +1,8 @@
 #include "tokens.h"
+#include "structure.h"
 
 #if !defined(_SELECT_EXPR_H_)
     #define _SELECT_EXPR_H_ 1
-    int expr_column_expr(token_array_t *tokens,int depth);
+    int expr_column_expr(token_array_t *tokens,int depth,select_t *sel);
 
 #endif
