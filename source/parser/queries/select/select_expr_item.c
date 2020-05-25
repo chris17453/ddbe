381816c3fc969070aace3981916f861ed46e3c16
Charles Watkins <chris17453@gmail.com>
2020-05-25 16:40:56 -0400

    Commiting Build 1.0.179

--- source/parser/queries/select/select_expr_item.c
+++ source/parser/queries/select/select_expr_item.c
@@ -19,7 +19,7 @@ int expr_column_expr(token_array_t *tokens,int depth,select_t *sel){
         
         sel->columns[index].alias=0;
         sel->columns[index].alias="F";
-        if(expr_alias(tokens,depth,sel->columns[index].alias)){ //sel->columns[sel->column_length].alias
+        if(expr_alias(tokens,depth,&sel->columns[index].alias)){ //sel->columns[sel->column_length].alias
             //sel->columns[index].alias="BOB";
             goop(depth,"alias","found");
         }
