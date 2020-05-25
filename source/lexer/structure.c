d55b8fa3a3c398d60931cfdab36e0255efc5da95
Charles Watkins <chris17453@gmail.com>
2020-05-25 14:23:32 -0400

    Commiting Build 1.0.79

--- source/lexer/structure.c
+++ source/lexer/structure.c
@@ -50,13 +50,13 @@ void select_add_column(select_t *obj){
     // create
     printf("new columns\n");
     data_column_t *new_columns=data_column_list_init(obj->column_length+1);
-    
+    memset(&new_columns,sizeof(data_column_t)*obj->column_length+1);
     // if existing items exist
     if(obj->columns!=0) {
         printf("old columns\n");
         // copy
         int data_size=sizeof(data_column_t)*obj->column_length;
-        memcpy(new_columns,obj->columns,data_size);
+        //memcpy(new_columns,obj->columns,data_size);
         // destroy old
         free(obj->columns);
     }
