ca769e71b71e9e370cbbebc1f067ab92440bdfcd
Charles Watkins <chris17453@gmail.com>
2020-05-25 14:29:01 -0400

    Commiting Build 1.0.91

--- source/lexer/structure.c
+++ source/lexer/structure.c
@@ -73,7 +73,7 @@ void select_add_column(select_t *obj){
     
     printf("column init\n");
     //init the newest column
-    data_column_init(&obj->columns[obj->column_length]);
+    data_column_init(obj->columns[obj->column_length]);
 
     ++obj->column_length;
    // select_debug(obj);
