0632364c6584df872e3c78fcff81b233b735a4cc
Charles Watkins <chris17453@gmail.com>
2020-05-25 14:14:07 -0400

    Commiting Build 1.0.72

--- source/parser/queries/select/select_expr_item.c
+++ source/parser/queries/select/select_expr_item.c
@@ -13,10 +13,12 @@ int expr_column_expr(token_array_t *tokens,int depth,select_t *sel){
     int pos=tokens->position;
     if(expr_simple_expr(tokens,depth)){
         select_add_column(sel);
-        sel->columns[sel->column_length].type=1;
-        sel->columns[sel->column_length].ordinal=sel->column_length;
-        sel->columns[sel->column_length].alias="BOBO";
-        if(expr_alias(tokens,depth,sel->columns[sel->column_length].alias)){
+        //set_column()
+        //sel->columns[sel->column_length].type=1;
+        //sel->columns[sel->column_length].ordinal=sel->column_length;
+        //sel->columns[sel->column_length].alias="BOBO";
+        char *bob;
+        if(expr_alias(tokens,depth,bob)){
             goop(depth,"alias","found");
         }
         ++sel->column_length;
