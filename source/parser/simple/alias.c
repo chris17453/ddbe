b3f05ddd7d9dee3b1dbcf59056e5996aadb3a327
Charles Watkins <chris17453@gmail.com>
2020-05-25 16:40:39 -0400

    Commiting Build 1.0.178

--- source/parser/simple/alias.c
+++ source/parser/simple/alias.c
@@ -14,7 +14,7 @@ int expr_alias(token_array_t *tokens,int depth,char **target){
         if(compare_token(tokens,0,TOKEN_ALPHA)){        
             target=&tokens->array[tokens->position-1].value;
             //target=&tokens->array[tokens->position-1].value;
-            set_alias(tokens,tokens->position-1,target);
+         //   set_alias(tokens,tokens->position-1,target);
             return 1;
         }
 
