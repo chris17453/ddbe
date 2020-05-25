3f601f7f2f2e3da6fafd38385649a0f030c4395b
Charles Watkins <chris17453@gmail.com>
2020-05-25 19:22:35 -0400

    Commiting Build 1.0.331

--- source/parser/queries/select/order_by.c
+++ source/parser/queries/select/order_by.c
@@ -21,6 +21,7 @@ int expr_order_by(token_array_t *tokens,int depth,select_t* sel){
             while(looper){
                 pos2=tokens->position;
                 if(!expr_identifier(tokens,depth)){
+                    goop(depth,"ORDER-BY","NO IDENTIFIER");
                     //no select epression
                     break;
                 }
@@ -31,10 +32,12 @@ int expr_order_by(token_array_t *tokens,int depth,select_t* sel){
                 
                 } else 
                     if(compare_token(tokens,0,TOKEN_DESC)) {
+                        add_order_column(sel);
                         sel->order[sel->order_length-1].identity=&tokens->object;
                         sel->order[sel->order_length-1].direction=TOKEN_DESC;
 
                     }  else {
+                        add_order_column(sel);
                         sel->order[sel->order_length-1].identity=&tokens->object;
                         sel->order[sel->order_length-1].direction=TOKEN_ASC;
                     }
