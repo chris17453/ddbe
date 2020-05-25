a3c498532acc6d8486fef55b26b5133685a580f0
Charles Watkins <chris17453@gmail.com>
2020-05-25 14:06:43 -0400

    Commiting Build 1.0.63

--- source/core/core.c
+++ source/core/core.c
@@ -4,8 +4,9 @@
 void * safe_malloc(int size,int qty){
     int length=size*qty;
     if(length<=0) ghost(ERR_OUT_OF_BOUNDS);
-    void *ptr;
-    ptr=malloc(length);
+    printf("malloc\n");
+    void *ptr=malloc(length);
+    printf("malloc2\n");
     if(ptr==0) {
         ghost(ERR_MEMORY_ALLOCATION_ERR);
     }
