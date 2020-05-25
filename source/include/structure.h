a633080692e609539766ca33f94696ce63dcf831
Charles Watkins <chris17453@gmail.com>
2020-05-25 19:14:06 -0400

    Commiting Build 1.0.324

--- source/include/structure.h
+++ source/include/structure.h
@@ -29,36 +29,36 @@ typedef struct group_column {
 
 typedef struct select_t{
         // sub elements
-        data_column_t *columns;
-        identifier_t  *from;
-        void          *where;
-        void          *order;
-        void          *group;
+        data_column_t   * columns;
+        identifier_t    * from;
+        void            * where;
+        order_column_t  * order;
+        void            * group;
         
         // elements
-        char          *alias;
-        int            distinct;
-        int            limit_start;
-        int            limit_length;
+        char          * alias;
+        int             distinct;
+        int             limit_start;
+        int             limit_length;
 
         // internal
-        int            column_length;     
-        int            order_length;     
-        int            group_length;     
-        int            where_length;     
-        int            has_limit_length;
-        int            has_limit_start;
+        int             column_length;     
+        int             order_length;     
+        int             group_length;     
+        int             where_length;     
+        int             has_limit_length;
+        int             has_limit_start;
     } select_t;
     
 
-    select_t       *select_new();
-    data_column_t  *data_column_list_init(int length);
-    void            add_data_column(select_t *obj);
-    void            data_column_init(data_column_t *column);
-    order_column_t *order_column_list_init(int length);
-    void            order_column_init(order_column_t *column);
-    void            add_order_column(select_t *obj);
-    void            set_distinct(select_t *obj);
-    void            select_debug(select_t *obj);
+    select_t       * select_new();
+    data_column_t  * data_column_list_init(int length);
+    void             add_data_column(select_t *obj);
+    void             data_column_init(data_column_t *column);
+    order_column_t * order_column_list_init(int length);
+    void             order_column_init(order_column_t *column);
+    void             add_order_column(select_t *obj);
+    void             set_distinct(select_t *obj);
+    void             select_debug(select_t *obj);
 
 #endif
