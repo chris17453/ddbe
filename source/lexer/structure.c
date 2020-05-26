792c7b9147b7fc228c8cc975cc79850d13c27640
Charles Watkins <chris17453@gmail.com>
2020-05-25 23:21:53 -0400

    Commiting Build 1.0.402

--- source/lexer/structure.c
+++ source/lexer/structure.c
@@ -249,7 +249,7 @@ void select_debug(select_t *obj){
             for(int w=0;w<where->length;w++) {
                 printf(" -expr-------%s\n");
                 token_t *token=&where->tokens[w];
-                printf("     %s,val: %s\n",token_type(token->type),token->value );
+                printf("     %s\n",token_type(token->type) );
             }
         }
     }
