5b67311fbae034b099ff919fae2d52afdad8f56a
Charles Watkins <chris17453@gmail.com>
2020-05-25 23:49:23 -0400

    Commiting Build 1.0.425

--- source/parser/queries/select/from.c
+++ source/parser/queries/select/from.c
@@ -11,13 +11,9 @@ int expr_from(token_array_t *tokens,int depth,select_t* sel){
     int pos=tokens->position;
     
     if(compare_token(tokens,0,TOKEN_FROM)){
-        goop(depth,"SELECT","BEFORE FROM-ident");
-
         if(expr_identifier(tokens,depth)){        
-            goop(depth,"SELECT","BEFORE FROM-alias");
             sel->from=tokens->object;
             if(expr_alias(tokens,depth,&sel->alias)) {
-                goop(depth,"FORM","Found Alias");
             }
             
             set_from(tokens,sel);
