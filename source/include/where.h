82e889896e061ede968ebae91522b8228c262b56
Charles Watkins <chris17453@gmail.com>
2020-05-25 20:47:00 -0400

    Commiting Build 1.0.364

--- source/include/where.h
+++ source/include/where.h
@@ -1,9 +1,10 @@
 #include "tokens.h"
+#include "structure.h"
 
 #if !defined(_WHERE_H_)
     #define _WHERE_H_ 1
 
-    int expr_where(token_array_t *tokens,int depth);
+    int expr_where(token_array_t *tokens,int depth,select_t *tokens);
 
 
 #endif
