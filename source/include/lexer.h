608bc5be8f62c7f8f450a3b365c2b792dec17465
Charles Watkins <chris17453@gmail.com>
2020-05-26 15:08:35 -0400

    Commiting Build 1.0.454

--- source/include/lexer.h
+++ source/include/lexer.h
@@ -6,6 +6,7 @@
     #define _LEXER_H_ 1
     
     token_array_t *lex(char * query);
+    void token_combine(token_array_t *tokens,int *list);
     void consolidate_tokens(token_array_t *tokens);
     void process_queries(token_array_t *tokens);
     void print_token_range(token_array_t *tokens,char * prefix,int start,int end);
