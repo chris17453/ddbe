e004e90ffce416793d4d14695e35b5779a382831
Charles Watkins <chris17453@gmail.com>
2020-05-26 15:39:59 -0400

    Commiting Build 1.0.463

--- source/parser/queries/select/join.c
+++ source/parser/queries/select/join.c
@@ -18,25 +18,18 @@ int expr_join(token_array_t *tokens,int depth,select_t *sel){
     }
     ++tokens->position;
 
-    if(compare_token(tokens,0,TOKEN_ALPHA)){
-        goop(depth,"join identity",tokens->array[tokens->position].value);    
-    } else {
+    if(!compare_token(tokens,0,TOKEN_ALPHA)){
         ghost(ERR_INVALID_JOIN_IDENTITY);
     }
 
-    goop(depth,"join pre AS",tokens->array[tokens->position].value);
     if(compare_token(tokens,0,TOKEN_AS)){
-            goop(depth,"join as",tokens->array[tokens->position].value);
         if(compare_token(tokens,0,TOKEN_ALPHA)){
-            goop(depth,"join alias",tokens->array[tokens->position].value);
         } else {
             ghost(ERR_INVALID_JOIN_ALIAS);
         }
     }
 
-    goop(depth,"join pre on",tokens->array[tokens->position].value);
     if(compare_token(tokens,0,TOKEN_ON)) {
-        goop(depth,"join on",tokens->array[tokens->position].value);
         if(expr_expr(tokens,depth,sel)){
             tokens->object=0;
             return 1;
@@ -46,8 +39,6 @@ int expr_join(token_array_t *tokens,int depth,select_t *sel){
     } else {
         ghost(ERR_JOIN_WITHOUT_ON);
     }
-    goop(depth,"join fail",tokens->array[tokens->position].value);
-
     return 0;
 }
     
