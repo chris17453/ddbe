d0bffc2f9583812af702070b7e4a7c3e750f02c8
Charles Watkins <chris17453@gmail.com>
2020-05-25 23:51:41 -0400

    Commiting Build 1.0.429

--- source/parser/complex/bit_expr.c
+++ source/parser/complex/bit_expr.c
@@ -25,7 +25,6 @@ int expr_bit_expr(token_array_t *tokens,int depth){
 
     if(expr_simple_expr(tokens,depth)){
         int pos=tokens->position;
-        goop(depth,"bit-exp","got a simple");
         switch(tokens->array[tokens->position].type){
             case TOKEN_BIT_OR : break;
             case TOKEN_BIT_AND : break;
@@ -37,22 +36,21 @@ int expr_bit_expr(token_array_t *tokens,int depth){
             case TOKEN_DIVIDE : break;
             case TOKEN_MODULUS : break;
             default: 
-                goop(depth,"bit-exp",tokens->array[tokens->position].value);
-                goop(depth,"bit-exp","No bit operation");
+                //goop(depth,"bit-exp",tokens->array[tokens->position].value);
+                //goop(depth,"bit-exp","No bit operation");
                 return 1;
         }
-        goop(depth,"bit-exp",tokens->array[tokens->position].value);
-        goop(depth,"bit-exp","got a bit thing");
+        //goop(depth,"bit-exp",tokens->array[tokens->position].value);
+        //goop(depth,"bit-exp","got a bit thing");
 
         ++tokens->position;
         if(expr_simple_expr(tokens,depth)) {
-            goop(depth,"bit-exp","got a simple");
+           // goop(depth,"bit-exp","got a simple");
             return 1;
         }
         tokens->position=pos;
         return 1;
     }
-    goop(depth,"bit-exp","got NOTHING");
         
     return 0;
 }
