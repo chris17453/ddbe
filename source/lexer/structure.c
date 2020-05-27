2312b59d085d9dde70d7324f584999af549d0642
Charles Watkins <chris17453@gmail.com>
2020-05-26 22:40:30 -0400

    Commiting Build 1.0.530

--- source/lexer/structure.c
+++ source/lexer/structure.c
@@ -172,12 +172,12 @@ void set_distinct(select_t *obj){
 void select_debug(token_array_t *tokens){
 
     for(int i=0;i<tokens->position;i++){
-        char *t_type=tokens_type(tokens->array[i].type);
-        char *t_value=tokens_type(tokens->array[i].value);
+        char *t_type=token_type(tokens->array[i].type);
+        char *t_value=token_type(tokens->array[i].value);
 
         printf(" %s - %s  ",t_value,t_type);
         for(int e=0;e<tokens->array[i].depth;e++){
-            t_type=tokens_type(tokens->array[i].expr[e]);
+            t_type=token_type(tokens->array[i].expr[e]);
             printf(" %s ",t_type);
         }
         printf("\n");
