fda0349b8c0b8843a45b5070cb88758988342b32
Charles Watkins <chris17453@gmail.com>
2020-05-26 22:15:20 -0400

    Commiting Build 1.0.512

--- source/include/from.h
+++ source/include/from.h
@@ -3,7 +3,7 @@
 
 #if !defined(_FROM_H_)
     #define _FROM_H_ 1
-    int expr_from(token_array_t *tokens,int depth,select_t *sel);
-    void set_from(token_array_t *tokens,select_t* sel);
+
+    int expr_from(token_array_t *tokens,int depth);
 
 #endif
--- source/include/group_by.h
+++ source/include/group_by.h
@@ -4,7 +4,7 @@
 #if !defined(_GROUP_BY_H_)
 
     #define _GROUP_BY_H_ 1
-    int expr_group_by(token_array_t *tokens,int depth,select_t* sel);
+    int expr_group_by(token_array_t *tokens,int depth);
 
 #endif
 
