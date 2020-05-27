20038488a63f8da451ffaae50ae276d1e555d772
Charles Watkins <chris17453@gmail.com>
2020-05-26 22:18:59 -0400

    Commiting Build 1.0.519

--- source/parser/complex/where.c
+++ source/parser/complex/where.c
@@ -12,7 +12,7 @@ int expr_where(token_array_t *tokens,int depth){
     int pos=tokens->position;
 
     if(compare_token(tokens,0,TOKEN_WHERE)){
-        if(expr_expr(tokens,depth,sel)){
+        if(expr_expr(tokens,depth)){
             token_add_type_range(tokens,TOKEN_WHERE,pos);
 
             return 1;
@@ -22,20 +22,3 @@ int expr_where(token_array_t *tokens,int depth){
     tokens->position=pos;
     return 0;
 }
-
-/*
-where_expr_t * where_expr_new(){
-        int length=tokens->position-pos-1;
-        printf("length %d",length);
-        sel->where=safe_malloc(sizeof(where_expr_t),1);
-
-        sel->where->tokens=&tokens->array[pos+1];
-        sel->where->length=length;
-        sel->where->ordinal=1;
-        ++sel->where_length;
-
-
-    }        
-
-
-*/
