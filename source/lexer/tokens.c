d8648544b7e1f7f733f58c6808e4679a5a56983e
Charles Watkins <chris17453@gmail.com>
2020-05-27 20:35:29 -0400

    Commiting Build 1.0.626

--- source/lexer/tokens.c
+++ source/lexer/tokens.c
@@ -154,7 +154,7 @@ void token_set_type(token_array_t * arr,int type,int index){
 
 void tokens_destroy(token_array_t *tokens){
     for(int i=0;i<tokens->length;i++){
-        free(tokens->array[i].value);
+        if(tokens->array[i].value!=0) free(tokens->array[i].value);
     }
     free(tokens->array);
     free(tokens);
