6a70a0dbafe1c9c193923c848e01e1e666118546
Charles Watkins <chris17453@gmail.com>
2020-05-29 12:18:35 -0400

    Commiting Build 1.0.741

--- source/parser/queries/select/select.c
+++ source/parser/queries/select/select.c
@@ -69,18 +69,18 @@ int match_pattern(token_array_t *tokens,int *pattern,int position){
 
 identifier_t * process_identifier(token_array_t *tokens,int *index){
     identifier_t *ident;
-    switch(token_at(tokens,index)->type) {
+    switch(token_at(tokens,*index)->type) {
         case TOKEN_QUALIFIER:   ident=safe_malloc(sizeof(identifier_t),1);
                                 ident->qualifier=token_at(tokens,*index)->value;
                                 ident->source   =token_at(tokens,*index+1)->value;
-                                index+=2;
+                                *index+=2;
                                 return ident;
 
         case TOKEN_SOURCE:         
                             ident=safe_malloc(sizeof(identifier_t),1);
                             ident->qualifier=0;
                             ident->source   =token_at(tokens,*index)->value;
-                            ++index;
+                            ++*index;
                             return ident;
     }
     return 0;
