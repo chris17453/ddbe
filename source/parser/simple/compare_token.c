e8b7eefb8f733a5b70dd13d69cf192caa2e40c9e
Charles Watkins <chris17453@gmail.com>
2020-05-30 21:33:50 -0400

    Commiting Build 1.1.#PATCH_MIN

--- source/parser/simple/compare_token.c
+++ source/parser/simple/compare_token.c
@@ -4,10 +4,15 @@
 int compare_token(token_array_t *tokens,int optional,int token){
     if(tokens->position>=tokens->length) return 0;
 
-    if(tokens->array[tokens->position].type==token) {
+    token_t *t=&tokens->array[tokens->position];
+
+    if(t==0) {
+        return 0;
+    }
+    if(t->type==token) {
         ++tokens->position;
         return 1;
-    }
+    } 
     if(optional==1) {
         return 1;
     }
