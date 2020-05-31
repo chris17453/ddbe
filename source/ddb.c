f68beb60253cbac49fe673858a32138ee7f63b56
Charles Watkins <chris17453@gmail.com>
2020-05-30 21:41:35 -0400

    Commiting Build 1.1.4

--- source/ddb.c
+++ source/ddb.c
@@ -47,7 +47,7 @@ int main(int argc, char* argv[]) {
 
     token_array_t *tokens=lex(query_str);
     free(query_str);
-
+    printf ("PROCESSING\n");
     process_queries(tokens);
     if(opt_debug) debug(tokens);
     if(opt_print_tokens) token_print(tokens);
