371336b4bd71bbed4f4fa85c59bc5b6fca95c93f
Charles Watkins <chris17453@gmail.com>
2020-05-31 10:35:23 -0400

    Commiting Build 1.1.116

--- source/parser/queries/select/select.c
+++ source/parser/queries/select/select.c
@@ -22,15 +22,15 @@ expression_t  * process_boolean_primary  (token_array_t *tokens,int *index);
 expression_t  * process_expression       (token_array_t *tokens,int *index);
 expression_t  * process_group_column_list(token_array_t *tokens,int *index);
 expression_t  * process_order_column_list(token_array_t *tokens,int *index);
-void            process_select(token_array_t *tokens,int *start);
+void            process_select           (token_array_t *tokens,int *start);
 // cleaners
-int             select_free(select_t select) ;
-int             free_string(char *data);
-int             free_expression(expression_t *expr);
-int             free_ident(identifier_t *ident);
-int             free_litteral(token_t *token);
+int             select_free              (select_t select) ;
+int             free_string              (char *data);
+int             free_expression          (expression_t *expr);
+int             free_ident               (identifier_t *ident);
+int             free_litteral            (token_t *token);
 // debuggers
-void            select_print(select_t select);
+void            select_print             (select_t select);
 void            debug_expr               (expression_t *expr,int depth);
 
 
