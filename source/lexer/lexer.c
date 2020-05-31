7a332776d195580422e2c0f075d3230956741319
Charles Watkins <chris17453@gmail.com>
2020-05-30 21:07:44 -0400

    Commiting Build 1.0.992

--- source/lexer/lexer.c
+++ source/lexer/lexer.c
@@ -387,21 +387,21 @@ void consolidate_tokens(token_array_t *tokens){
     // limit fixup
     for(int i=0;i<tokens->top;i++) {
         if(tokens->array[i].type== TOKEN_LIMIT) {
-            //token_delete(tokens,i);
-i++;
+            token_delete(tokens,i);
+
             if(
                 tokens->array[i  ].type==TOKEN_NUMERIC &&
                 tokens->array[i+1].type==TOKEN_LIST_DELIMITER && 
                 tokens->array[i+2].type==TOKEN_NUMERIC
                 ) {
-                    tokens->array[i  ].type==TOKEN_LIMIT_START;
-                    tokens->array[i+2].type==TOKEN_LIMIT_LENGTH;
+                    tokens->array[i  ].type=TOKEN_LIMIT_START;
+                    tokens->array[i+2].type=TOKEN_LIMIT_LENGTH;
                     token_delete(tokens,i+1);
                     ++i;
                     continue;
                 }
-            if( tokens->array[i].type==TOKEN_NUMERIC){
-                tokens->array[i].type==TOKEN_LIMIT_LENGTH;
+            if( tokens->array[i].type=TOKEN_NUMERIC){
+                tokens->array[i].type=TOKEN_LIMIT_LENGTH;
             }
         }
     }//end limit fixup
